38759ee76cd5b332ffa044a7a287094aa7905a9a
Nick <nv314961@bu.edu>
2017-10-25 18:30:05 -0400

    Source Directory Update
    - Added Bootstrap source files to directory
    - Removed Bootstrap Studio file and the "assets" folder that Bootstrap Studio generated.
    - Firebase integration in progress

---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+// Initializes DMC login page.
+function DMC() {
+  this.checkSetup();
+  
+  }
+  
+// Checks that the Firebase SDK has been correctly setup and configured.
+DMC.prototype.checkSetup = function() {
+  if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
+    window.alert('You have not configured and imported the Firebase SDK. ' +
+        'Make sure you go through the codelab setup instructions and make ' +
+        'sure you are running the codelab using `firebase serve`');
+  }
+};
+
+// Sets up shortcuts to Firebase features and initiate firebase auth.
+DMC.prototype.initFirebase = function() {
+  // Shortcuts to Firebase SDK features.
+  this.auth = firebase.auth();
+  this.database = firebase.database();
+  this.storage = firebase.storage();
+  // Initiates Firebase auth and listen to auth state changes.
+  this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
+};
+
+// Signs-in DMC.
+DMC.prototype.signIn = function() {
+  // Sign in Firebase using popup auth and Google as the identity provider.
+  var provider = new firebase.auth.GoogleAuthProvider();
+  this.auth.signInWithPopup(provider);
+};
+
+// Signs-out of DMC.
+DMC.prototype.signOut = function() {
+  // Sign out of Firebase.
+  this.auth.signOut();
+};
